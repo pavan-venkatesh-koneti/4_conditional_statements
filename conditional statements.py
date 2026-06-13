ff2339071801b36b6460f2404c90b50f0e31f8c8
PAVAN VENKATESH KONETI <pavankoneti85@gmail.com>
2026-06-13 17:26:06 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,756 @@
+'''simple if'''
+
+'''check positive number'''
+# a=int(input("enter the number:-"))
+# if a>0:
+#     print("positive")
+ 
+'''check negative number'''
+# a=int(input("enter the number:-"))
+# if a<0:
+#     print("negative")
+
+'''check zero'''
+# a=int(input("enter the number:-"))
+# if a==0:
+#     print("zero")
+
+'''check even number'''
+# a=int(input("enter the number:-"))
+# if a%2==0:
+#     print("even")
+
+'''check oddnumber'''
+# a=int(input("enter the number:-"))
+# if a%2!=0:
+#     print("0dd")
+
+'''check eligible to vote'''
+# a=int(input("enter the number:-"))
+# if a>18:
+#     print("eligible to vote")
+
+'''check student is pass'''
+# a=int(input("enter the number:-"))
+# if a>=35:
+#     print("student is pass")
+
+'''divisible by 5'''
+# a=int(input("enter the number:-"))
+# if a%5==0:
+#     print("divisible by 5")
+
+'''divisible by 10'''
+# a=int(input("enter the number:-"))
+# if a%10==0:
+#     print("divisible by 10")
+
+'''check vowel'''
+# a=input("enter the character:-")
+# if a in "aeiouAEIOU":
+#     print("vowel")
+
+'''if -else'''
+
+'''even or odd'''
+# a=int(input("enter the number:-"))
+# if a%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+'''positive or negative '''
+# a=int(input("enter the number:-"))
+# if a>0:
+#     print("positive")
+# else:
+#     print("negative")
+
+'''pass or fail'''
+# a=int(input("enter the number:-"))
+# if a>=35:
+#     print("pass")
+# else:
+#     print("fail")
+
+'''major or minor'''
+# a=int(input("enter the number:-"))
+# if a>=18:
+#     print("major")
+# else:
+#     print("minor")
+    
+'''divisible by 7 or not'''
+# a=int(input("enter the number:-"))
+# if a%7==0:
+#     print("divisible")
+# else:
+#     print("not divisible")
+
+'''leap year or not'''
+# feb =int(input("enter the days in feb :-"))
+# if feb==29:
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+'''upper or lower cases'''
+# a=input("enter the character:-")
+# if a>='A'  and a<='Z':
+#     print("UPPER CASE")
+# else:
+#     print("lower case")
+
+'''multiple of 3 or not'''
+# a=int(input("enter the number:-"))
+# if a%3==0:
+#     print("multiple of 3")
+# else:
+#     print("not multiple os 3")
+
+'''greater than 100'''
+# a=int(input("enter the number:-"))
+# if a>100:
+#     print("greater than 100")
+# else:
+#     print(" not greater than 100")
+
+'''get driving license or not'''
+# a=int(input("enter the number:-"))
+# if a>18:
+#     print("get driving license")
+# else:
+#     print(" not get driving license")
+
+'''if-elif-else'''
+
+'''lergest of 2 numbers'''
+# a=int(input("enter the number:-"))
+# b=int(input("enter the number:-"))
+# if a>b:
+#     print(a,"is largest")
+# else:
+#     print(b,"is largest")
+    
+'''lergast of 3 numbers'''
+# a=int(input("enter the number 1:-"))
+# b=int(input("enter the number 2:-"))
+# c=int(input("enter the number 3:-"))
+# if a>b and a>c:
+#     print(a,"is largest")
+# elif b>c:
+#     print(b,"is largest")
+# else:
+#     print(c,"is largest")
+
+'''smallest of 3 numbers'''
+# a=int(input("enter the number 1:-"))
+# b=int(input("enter the number 2:-"))
+# c=int(input("enter the number 3:-"))
+# if a<b and a<c:
+#     print(a,"is smallest")
+# elif b<c:
+#     print(b,"is smallest")
+# else:
+#     print(c,"is smallest")
+
+'''positive or negative or zero'''
+# a=int(input("enter the number :"))
+# if a>0:
+#     print(a,"is positive")
+# elif a<0:
+#     print(a,"is negative")
+# else:
+#     print(a,"is zero")
+
+'''students grades '''
+# marks=int(input('enter the marks :-'))
+# if marks>=85:
+#     print('a grade')
+# elif marks>=65:
+#     print('b grade')
+# elif marks>=35:
+#     print('c grade')
+# else:
+#     print('fail')
+
+''''vowel or consonant or digit or special character'''
+# value=input("enter the value:")
+# if value in "aeiouAEIOU":
+#     print("vowel")
+# elif (value>="A" and value<="Z") or (value>="a" and value<="z"):
+#     print("consonant")
+# elif value>='0' and value<='9':
+#     print("digit")
+# else:
+#     print("special character")
+
+'''simple calculator'''
+# number1=float(input("enter the number 1:"))
+# number2=float(input("enter the number 2:"))
+
+# print("available operations")
+# print("1.add")
+# print("2.sub")
+# print("3.mul")
+# print("4.div")
+# choice=input("enter the choice(1/2/3/4):")
+# if choice=="1":
+#     print("value",number1+number2)
+# elif choice=="2": 
+#     print("value",number1-number2) 
+# elif choice=="3": 
+#     print("value",number1*number2)
+# elif choice=="4":
+#     if number2!=0:
+#         print("value",number1/number2)
+#     else:
+#         print("error,zero cant divided")
+# else:
+#     print("invald choice")
+
+'''week names'''
+# num=int(input("enter the number from 1 to 7:-"))
+# if num==1:
+#     print("monday")
+# elif num==2:
+#     print("tuesday")
+# elif num==3:
+#     print("wednesday")
+# elif num==4:
+#     print("thursday")
+# elif num==5:
+#     print("friday")
+# elif num==6:
+#     print("saturday")
+# elif num==7:
+#     print("sunday")
+# else:
+#     print("invalid")
+
+'''month names by month numbers'''
+# month = int(input("Enter month number (1-12): "))
+
+# if month == 1:
+#     print("January")
+# elif month == 2:
+#     print("February")
+# elif month == 3:
+#     print("March")
+# elif month == 4:
+#     print("April")
+# elif month == 5:
+#     print("May")
+# elif month == 6:
+#     print("June")
+# elif month == 7:
+#     print("July")
+# elif month == 8:
+#     print("August")
+# elif month == 9:
+#     print("September")
+# elif month == 10:
+#     print("October")
+# elif month == 11:
+#     print("November")
+# elif month == 12:
+#     print("December")
+# else:
+#     print("Invalid month number")
+
+'''ticket price by there age'''
+# age= int (input("enter the age :-"))
+# if age<=6:
+#     print("ticket price is 20")
+# elif age>6 and age<=18:
+#     print("ticket price is 40")
+# elif age>18 and age<=65:
+#     print("ticket price is 70")
+# else:
+#     print("ticket price is 50")
+
+'''multiple conditions'''
+
+'''divisible by 3 and 5'''
+# a=int(input("enter the number:-"))
+# if a%3==0 and a%5==0:
+#     print("divisible by 3 and 5")
+# else:
+#     print("not divisible")
+
+'''divisible by 3 or 5'''
+# a=int(input("enter the number:-"))
+# if a%3==0 or a%5==0:
+#     print("divisible by 3 or 5")
+# else:
+#     print("not divisible")
+    
+'''eligibe for loan or not'''  
+# age=int(input("enter the age :-"))
+# income=int(input("enter the income:-"))
+# if age> 18 and income>30000:
+#     print ("eligible for loan")
+# else:
+#     print("not eligile for loan")
+
+'''login system'''
+# username=input("enter username:-")
+# password=int(input("enter password:-"))
+# if username=="pavan" and password==2005:
+#     print("login successful")
+# else:
+#     print("incorrect data")
+
+''' student grading'''
+# marks=int(input('enter the marks :-'))
+# if marks>=85:
+#     print('a grade')
+# elif marks>=65:
+#     print('b grade')
+# elif marks>=35:
+#     print('c grade')
+# else:
+#     print('fail')
+    
+'''number between 10 to 50'''
+# num=int(input("enter the number :-"))
+# if num>=10 and num <=50:
+#     print (num,"in between 10 to 50")
+# else:
+#     print("not in between 10 to 50")
+
+'''year belongs to 20th or 21st century'''
+# year=int(input("enter the year:-"))
+# if year>=1901 and year<=2000:
+#     print(year,"is belongs to 20th century")
+# elif  year>=2001 and year<=3000:
+#     print(year,"is belongs to 21st century")
+# else:
+#     print(year,"is belongs to some other time line")
+
+'''number digits count'''
+# num= int(input("enter the number:-"))
+# if num>0 and num<=9:
+#     print(num,"is single digit number")
+# elif num>9 and num<=99:
+#     print(num,"is two digit number")
+# elif num>99 and num<=999:
+#     print(num,"is three digits number")
+# elif num>999 and num<=9999:
+#     print(num,"is four digits number")
+# else:
+#     print(num,"is more than four digits")
+
+'''elgibile for discont by age'''
+# age=int(input("enter the age :-"))
+# if age<10 or age>75:
+#     print ("eligible for discont")
+# else:
+#     print("not eligible for discont")
+
+'''triangle is valid using three angles'''
+# angle_1=int(input("enter the first angle:-"))
+# angle_2=int(input("enter the second angle:-"))
+# angle_3=int(input("enter the third angle:-"))
+# a=angle_1+angle_2+angle_3
+# if a==180:
+#     print("the three angles make triangle")
+# else:
+#     print("not make triangle")
+
+'''simple clacultor'''
+# a=12
+# b=2
+# op=input("enter the operator")
+# if op=="+":
+#     print("a+b")
+# if op=="-":
+#     print("a-b")
+
+# n=5360
+# if n>0:
+#     d1=n//500
+#     print("500 notes",d1)
+#     n=n%500
+# if n>0:
+#     d2=n//200
+#     print("200 notes",d2)
+#     n=n%200
+# if n>0:
+#     d3=n//100
+#     print("100 notes",d3)
+#     n=n%100
+# if n>0:
+#     d4=n//50
+#     print("50 notes",d4)
+#     n=n%50
+#     print ("remaing ruppes",n)  
+ 
+'''tax '''
+# n=1200000
+# if n==200000:
+#     print("no tax")
+# elif n>=200000 and n<=500000:
+#     tax1=(n-200000)*0.5
+#     print("tax1",tax1)
+# elif n>=500000 and n<=1000000:
+#     tax2=(n-200000)*0.10
+#     print("tax2",tax2)    
+# elif n>1000000:
+#     tax3=(n-200000)*0.15
+#     print("tax3",tax3) 
+# 
+'''nested if '''
+# age =12
+# citizen=True
+
+# if age>=18:
+#     if citizen:
+#         print("eligible")
+#     else:
+#         print("not a citizen")
+# else:
+#     print("not eligible")
+
+'''interviwe selection'''
+# cgpa=float(input("enter the cgpa:-"))
+# communication=bool(input("enter the value:-"))
+
+# if cgpa>=8.5:
+#     if communication:
+#         print("selected")
+#     else:
+#         print("less communication")
+# else:
+#     print("not selected")
+
+'''money withdraw'''
+# amount=10000
+# withdraw=float(input("enter the amount:-"))
+
+# if amount>withdraw:
+#     if withdraw<=5000:
+#         print("withdraw successfull")
+#     else:
+#         print("today limit cross")
+# else:
+#     print("insuffecent balance")
+
+'''largest  three numbers'''
+# a=float(input("enter the number 1:-"))
+# b=float(input("enter the number 2:-"))
+# c=float(input("enter the number 3:-"))
+# if a>b:
+#     if a>c:
+#         print(a,"a is largest number")
+#     elif b>c:
+#         print(b,"b is largest")
+# else:
+#     print(c,"c is largest")
+    
+'''smallest of three numbers'''
+# a=float(input("enter the number 1:-"))
+# b=float(input("enter the number 2:-"))
+# c=float(input("enter the number 3:-"))
+# if a<b:
+#     if a<c:
+#         print(a,"a is smallest number")
+#     elif b<c:
+#         print(b,"b is smallest number")
+# else:
+#     print(c,"c is smallest number")
+
+'''job eligible'''
+# cgpa=float(input("enter the cgpa:-"))
+# age=int(input("enter the age:-"))
+
+# if cgpa>=8.5:
+#     if age>=18:
+#         print("selected")
+#     else:
+#         print("less age")
+# else:
+#     print("not selected")
+
+'''all subject pass'''
+# subject1=int(input("enter the marks :-"))
+# subject2=int(input("enter the marks :-"))
+# subject3=int(input("enter the marks :-"))
+# a=subject1+subject2+subject3
+# if a>=105:
+#     if subject1>=35 and subject2>=35 and subject3>=35:
+#         print("pass all subjects")
+#     else:
+#         print("fail in some subject")
+# else:
+#     print("fail")
+
+'''ATM withdrawal'''
+# amount=10000
+# withdraw=float(input("enter the amount:-"))
+
+# if amount>withdraw:
+#     if withdraw<=5000:
+#         print("withdraw successfull")
+#     else:
+#         print("today limit cross")
+# else:
+#     print("insuffecent balance")
+
+'''online shopping discount'''
+# product=int(input("enter the value of product:-"))
+# membership=input("DO YOU HEVE MEMBERSHIP: yes or no :-")
+# coupon=input("DO YOU HAVE COUPON :yes or no :-")
+# if product>=200:
+#     if membership=="yes" and coupon=="yes":
+#         coupon1=(product-200)*0.20
+#         print(coupon1,"is discount for membership and coupon ")
+#     elif membership=="yes":
+#         membership1=(product-200)*0.10
+#         print(membership1,"is discount for membership")
+#     else:
+#         print("you dont have membership")
+# else:
+#     print("you dont have any discount")
+
+'''movie tickets'''
+# age=int(input("enter the age :-"))
+# tickets=int(input("enter the how many tickets:-"))
+# if age>=18:
+#     if tickets<=6:
+#         print("tickets available")
+#     else:
+#         print("tickets are not available")
+# else:
+#     print("age is not eligible")
+
+'''college admission'''
+# inter_marks=int(input("enter the inter marks:-"))
+# entrance_marks=int(input("enter the entrance marks:-"))
+# if inter_marks>=700:
+#     if entrance_marks>=60:
+#         print("admission granted")
+#     else:
+#         print("rewrite entrance exam")
+# else:
+#     print("sorry to say your inter marks are low so,admission failed")
+
+'''bonus for employe'''
+# salary=int(input("enter your salary:-"))
+# experience=int(input("enter experience :-"))
+# performance=input("performance (bad/good):-")
+# if experience>=2:
+#     if performance=="good":
+#         bonus=(salary)*0.02
+#         print(bonus,"is your bonus")
+#     else:
+#         print("your performance is not good")
+# else:
+#     print("you dont have experience")
+
+'''loan approval'''
+# age=int(input("enter the age :-"))
+# salary=int(input("enter the salary :-"))
+# credit_score=int(input("enter the credit score:-"))
+
+# if age>=18 and salary>=25000:
+#     if credit_score>800:
+#         print("loan granted")
+#     else:
+#         print("your credit score is low")
+# else:
+#     print("not eligible for loan")
+
+'''leap year'''
+# n=2014
+# if n%4==0 and n%100!=0:
+#     print("leap year")
+# elif n%400==0:
+#     print("leap year")
+# else:
+#     print("not leap year")
+
+'''electricity bill'''
+# units=int(input("enter the units :-"))
+# if units>1 and units<=50:
+#     a=units*2
+#     print(a,"is current bill")
+# elif units>50 and units<=100:
+#     b=units*3
+#     print(b,"is current bill")
+# elif units>100 and units<=150:
+#     c=units*4
+#     print(c,"is current bill")
+# else:
+#     print("is your current bill",units*10)
+    
+    
+'''two strings are equal or not'''
+# string1=input("enter string1:-")
+# string2=input("enter string2:-")
+# if string1==string2:
+#     print("yes strings are equal")
+# else:
+#     print("not equal")
+
+'''check number digits'''
+# num= int(input("enter the number:-"))
+# if num>0 and num<=9:
+#     print(num,"is single digit number")
+# elif num>9 and num<=99:
+#     print(num,"is two digit number")
+# elif num>99 and num<=999:
+#     print(num,"is three digits number")
+# elif num>999 and num<=9999:
+#     print(num,"is four digits number")
+# else:
+#     print(num,"is more than four digits")
+
+'''smallest of three numbers'''
+# a=int(input("enter the number 1:-"))
+# b=int(input("enter the number 2:-"))
+# c=int(input("enter the number 3:-"))
+# if a<b and a<c:
+#     print(a,"is smallest")
+# elif b<c:
+#     print(b,"is smallest")
+# else:
+#     print(c,"is smallest")
+
+'''largest  three numbers'''
+# a=float(input("enter the number 1:-"))
+# b=float(input("enter the number 2:-"))
+# c=float(input("enter the number 3:-"))
+# if a>b:
+#     if a>c:
+#         print(a,"a is largest number")
+#     elif b>c:
+#         print(b,"b is largest")
+# else:
+#     print(c,"c is largest")
+
+'''admission'''
+# marks=int(input("enter the inter marks:-"))
+# attendance=int(input("enter the entrance marks:-"))
+# if marks>=60:
+#     if attendance>=75:
+#         print("admission granted")
+#     else:
+#         print("low attendance")
+# else:
+#     print("sorry to say your marks are low so,admission failed")
+    
+'''loan eligiblity'''
+# age=int(input("enter the age :-"))
+# salary=int(input("enter the salary :-"))
+# credit_score=int(input("enter the credit score:-"))
+
+# if age>=21 and salary>=25000:
+#     if credit_score>800:
+#         print("loan granted")
+#     else:
+#         print("your credit score is low")
+# else:
+#     print("not eligible for loan")
+
+'''bonus eligiblity'''
+# salary=int(input("enter your salary:-"))
+# experience=int(input("enter experience :-"))
+# performance=(int(input("performance (1/2/3/4/5):-")))
+# if experience>=5:
+#     if performance>=4:
+#         bonus=(salary)*0.02
+#         print(bonus,"is your bonus")
+#     else:
+#         print("your performance is not good")
+# else:
+#     print("you dont have experience")
+
+'''triangle'''
+# angle_1=int(input("enter the first angle:-"))
+# angle_2=int(input("enter the second angle:-"))
+# angle_3=int(input("enter the third angle:-"))
+# a=angle_1+angle_2+angle_3
+# if a==180:
+#     print("the three angles make triangle")
+# else:
+#     print("not make triangle")
+
+'''login'''
+# username=input("enter username:-")
+# password=int(input("enter password:-"))
+# if username=="pavan":
+#     if password==2005:
+#      print("login successful")
+#     else:
+#         print("wrong password")
+# else:
+#     print("incorrect user name")
+
+''' number b/w 10 to 50'''
+# num=int(input("enter the number :-"))
+# if num>=10 and num <=50:
+#     print (num,"in between 10 to 50")
+# else:
+#     print("not in between 10 to 50")
+
+'''college admission'''
+# inter_marks=int(input("enter the inter marks:-"))
+# entrance_marks=int(input("enter the entrance marks:-"))
+# if inter_marks>=700:
+#     if entrance_marks>=60:
+#         print("admission granted")
+#     else:
+#         print("rewrite entrance exam")
+# else:
+#     print("sorry to say your inter marks are low so,admission failed")
+
+'''first character uppercase'''
+# name=input("enter your name:-")
+# if name[0]=="aeiouAEIOU":
+#     print(name[0],"is VOWEL")
+# else:
+#     print(name[0],"is not VOWEL")
+    
+'''check oddnumber'''
+# a=int(input("enter the number:-"))
+# if a%2!=0:
+#     print("0dd")
+'''movie tickets'''
+# age=int(input("enter the age :-"))
+# tickets=int(input("enter the how many tickets:-"))
+# if age>=18:
+#     if tickets<=6:
+#         print("tickets available")
+#     else:
+#         print("tickets are not available")
+# else:
+#     print("age is not eligible")
+
+# a=input("enthe the character:-")
+# if "a"<=a<="z":
+#     print("lower case")
+# else:
+#     print("not")
+    
+    
+# a=input("enthe the digit:-")
+# if "0"<=a<="9":
+#     print("digit")
+# else:
+#     print("not")
+
+# ch=input("enter the character:-")
+# if "A"<=ch<="Z":
+#     a=ord(ch)+32
+#     print(chr(a))
+# elif "a"<=ch<="z":
+#     a=ord(ch)-32
+#     print(chr(a))
+# elif "0"<=ch<="9":
+#     a=ch,
+#     print(a)
+# else:
+#     a=ord(ch)+1
+#     print(chr(a))
+
